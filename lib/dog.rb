0a1c4c8ba506746b981c700ec7d33aeb1ea872f4
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-08-18 10:39:45 +0300

    commit tree 6ba6d3f90c1435cadc90545dac31c1be8cca1bea

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
+    def name=(dog_name)
+        @this_dogs_name=dog_name
+    end
 
+    def name 
+        @this_dogs_name
+    end
 end
+
+mutina=Dog.new
+mutina.name="mutina"
+
+puts mutina.name
